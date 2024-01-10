e2df3f8d28abcf5c209726d0650fd6fd4384da58
Thorben Bender <bender.thorben0@gmail.com>
2024-01-10 20:53:33 +0100

    Linked List (With enums)

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,29 @@
-use rand::{self, Rng};
+use std::env;
+use std::rc::Rc;
 
-fn select_sort(numbers: &mut Vec<i32>) {
-    for i in 0..numbers.len() {
-        let mut smallest = i;
-        for j in i + 1..numbers.len() {
-            println!("Values i: {} j: {}", numbers[i], numbers[j]);
-            if numbers[j] < numbers[smallest] {
-                smallest = j;
-            }
-        }
-        if smallest == i {
-            numbers.swap(i, smallest);
-        }
-    }
-}
-
-fn bubble_sort(numbers: &mut Vec<i32>) {
-    for _ in 0..numbers.len() {
-        let mut swapped = false;
-        for j in 0..numbers.len() - 1 {
-            if numbers[j] > numbers[j + 1] {
-                numbers.swap(j + 1, j);
-                swapped = true;
-            }
-        }
-
-        if !swapped {
-            break;
-        }
-    }
-}
-
-fn merge(mut left: Vec<i32>, mut right: Vec<i32>) -> Vec<i32> {
-    let mut result = vec![];
-
-    while left.len() > 0 && right.len() > 0 {
-        if left[0] > right[0] {
-            result.push(right.remove(0));
-        } else {
-            result.push(left.remove(0));
-        }
-    }
-    if right.len() > 0 {
-        result.append(&mut right);
-    }
-    if left.len() > 0 {
-        result.append(&mut left);
-    }
-    return result;
-}
-
-fn sort(numbers: Vec<i32>) -> Vec<i32> {
-    if numbers.len() > 1 {
-        let middle = numbers.len() / 2;
-        let left = sort(numbers[0..middle].to_vec());
-        let right = sort(numbers[middle..numbers.len()].to_vec());
-
-        return merge(left, right);
-    } else {
-        return numbers;
-    }
+#[derive(PartialEq)]
+enum Node {
+    Node { value: i32, next: Rc<Node> },
+    None,
 }
 
 fn main() {
-    let numbers: Vec<i32> = (0..=1000000)
-        .map(|_| rand::thread_rng().gen_range(0..=10000))
-        .collect();
-    println!("{:#?}", numbers);
-    let sorted = sort(numbers);
-    println!("{:?}", sorted);
+    let mut list = Rc::new(Node::None);
+    env::args()
+        .skip(1)
+        .filter_map(|arg| arg.parse().ok())
+        .for_each(|num| {
+            let n = Rc::new(Node::Node {
+                value: num,
+                next: Rc::clone(&list),
+            });
+
+            list = n;
+        });
+
+    let mut ptr = list;
+    while let Node::Node { value, next } = &*ptr {
+        println!("{}", value);
+        ptr = next.clone();
+    }
 }
