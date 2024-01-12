ada587ee3936c80916e8eac465a938e9f0138564
Thorben Bender <bender.thorben0@gmail.com>
2024-01-12 15:34:54 +0100

    Use sort with left and right pointer for two_sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut nums = nums.into_iter().enumerate().collect::<Vec<(usize, i32)>>();
 
-    nums.sort_unstable_by_key(|&(_, n)| n);
+    nums.sort();
 
-    for (k, (i, ni)) in nums.iter().enumerate() {
-        match nums[k + 1..].binary_search_by_key(&(target - ni), |&(_, nj)| nj) {
-            Ok(jj) => return vec![*i as i32, nums[jj + k + 1].0 as i32],
-            Err(_) => {}
+    let (mut left, mut right) = (0, nums.len() - 1);
+
+    while left < right {
+        let sum = nums[left].1 + nums[right].1;
+
+        if sum == target {
+            return vec![nums[left].0 as i32, nums[right].0 as i32];
+        } else if sum > target {
+            right -= 1;
+        } else {
+            left += 1;
         }
     }
 
-    unreachable!("Error: this place should not be reachable");
     return vec![0, 0];
 }
 
